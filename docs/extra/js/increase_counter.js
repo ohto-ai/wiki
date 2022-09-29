0e8e0f32258cafd5df170c0032780f84553aed2a
Ohto-Ai <zhu.thatboy@outlook.com>
2022-09-30 02:16:47 +0800

    Add increase_counter

---
+++ docs/extra/js/increase_counter.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        let increment = target / 200
+        if(counter.hasAttribute('data-step'))
+            step = +counter.getAttribute('data-step')
+        const c = +counter.innerText
+        if(c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
+        }
+    }
+
+    updateCounter()
+})
