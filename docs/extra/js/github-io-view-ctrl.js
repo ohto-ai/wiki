7ae359ead77089b5e254722ad4125ded9ce90b4c
OhtoAi <zhu.thatboy@outlook.com>
2022-11-01 14:59:37 +0800

    Fix githubio-ctrl

--- docs/extra/js/github-io-view-ctrl.js
+++ docs/extra/js/github-io-view-ctrl.js
@@ -1,14 +1,14 @@
 if(document.domain.endsWith('github.io'))
 {
-    $('a[href$="#only-github-io"]').css('display', 'none')
-    $('img[src$="#only-github-io"]').css('display', 'none')
-    $('a[href$="#only-github-io-with-parent"]').parent().css('display', 'none')
-    $('img[src$="#only-github-io-with-parent"]').parent().css('display', 'none')
-}
-else
-{
     $('a[href$="#not-github-io"]').css('display', 'none')
     $('img[src$="#not-github-io"]').css('display', 'none')
     $('a[href$="#not-github-io-with-parent"]').parent().css('display', 'none')
     $('img[src$="#not-github-io-with-parent"]').parent().css('display', 'none')
+}
+else
+{
+    $('a[href$="#only-github-io"]').css('display', 'none')
+    $('img[src$="#only-github-io"]').css('display', 'none')
+    $('a[href$="#only-github-io-with-parent"]').parent().css('display', 'none')
+    $('img[src$="#only-github-io-with-parent"]').parent().css('display', 'none')
 }
